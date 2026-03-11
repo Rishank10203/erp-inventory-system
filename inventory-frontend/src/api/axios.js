84b374741184e1fb0f914c9b5a8cdab7db13f625
Rishank10203 <patelrishank@gmail.com>
2026-03-11 23:16:42 +0530

    fix API url for deployment

--- inventory-frontend/src/api/axios.js
+++ inventory-frontend/src/api/axios.js
@@ -1,19 +1,22 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api/",
 });
 
 // Request Interceptor
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("access");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("access");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+);
 
 // Response Interceptor
 API.interceptors.response.use(
