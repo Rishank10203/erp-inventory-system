3b2142233febc03b40ea6f4d57b23efaaf011945
Rishank10203 <patelrishank@gmail.com>
2026-03-11 23:28:35 +0530

    fix cors for vercel frontend

--- inventory_backend/inventory_backend/settings.py
+++ inventory_backend/inventory_backend/settings.py
@@ -50,8 +50,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -80,6 +80,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "https://erp-inventory-system-woad.vercel.app",
 ]
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = ["*"]
 
 WSGI_APPLICATION = 'inventory_backend.wsgi.application'
 ASGI_APPLICATION = 'inventory_backend.asgi.application'
