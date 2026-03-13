e2b243366a713f3ccf5a869415fb852fe7d54794
Rishank10203 <patelrishank@gmail.com>
2026-03-14 00:04:02 +0530

    Refactor: complete authentication and API flow refinements as requested

--- inventory_backend/inventory_backend/urls.py
+++ inventory_backend/inventory_backend/urls.py
@@ -105,8 +105,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # JWT
-    # path('api/token/', MyTokenObtainPairView.as_view()),
-    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view()),
 
     # accounts
